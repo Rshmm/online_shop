ae9178d030c0d67e6edb958ae2f108c94ad81301
arsham <arshsm.hajeb@gmail.com>
2024-04-06 17:42:44 +0330

    در این کامیت از دکوریتور استفاده شد و درست کار کردن ان چک شد

--- account/views.py
+++ account/views.py
@@ -3,13 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login, authenticate
-from django.shortcuts import redirect
+from account.helper.decorators import create_view_logout_required
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     # success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    @create_view_logout_required('/')
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
         valid = super().form_valid(form)
