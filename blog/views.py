0197bba59891edbd795e4ef92d042b1f26e5d75a
arsham <arshsm.hajeb@gmail.com>
2024-04-22 20:38:49 +0330

    اضافه کردن پیجینیتور به ویو

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator
+
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(post_list, 8)
+    page_number = request.GET.get('get', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog.html', {'posts': posts})
 
 
