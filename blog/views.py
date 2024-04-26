8efbc2ccbe774c3e0db349e1a05b06373ffede6b
arsham <arshsm.hajeb@gmail.com>
2024-04-26 13:33:00 +0330

    اضاقه کردن کامنت به ویو پست دیتیل

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,12 @@ def post_detail(request, year, month, day, post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
-    return render(request, 'post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'post_detail.html', {'post': post,
+                                                'comments' : comments,
+                                                'form' : form
+                                                })
     
 
 @require_POST
