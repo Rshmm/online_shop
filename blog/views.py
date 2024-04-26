085f49b02beea22b430539fb583d8a69d0a0a210
arsham <arshsm.hajeb@gmail.com>
2024-04-26 12:55:55 +0330

    اضافه کردن ویو کامنت

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
-from blog.models import Post
+from blog.models import Post,Comment
 from django.core.paginator import Paginator
+from blog.froms import CommentForm
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -15,11 +17,32 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-        post = get_object_or_404(Post,
-                                status=Post.Status.PUBLISHED,
-                                slug=post,
-                                publish__year=year,
-                                publish__month=month,
-                                publish__day=day)
-        return render(request, 'post_detail.html', {'post': post})
+    post = get_object_or_404(Post,
+                            status=Post.Status.PUBLISHED,
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+    return render(request, 'post_detail.html', {'post': post})
+    
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(post,id=post_id,status=Post.Status.PUBLISHED)
+    comment = None
+    # a comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # create a comment object without saving it to the database
+        comment = form.save(commit=False)
+        #assign the post to the comment
+        comment.post = post
+        # save the comment to the database
+        comment.save()
+    return render(request, 'comment.html',
+                  {
+                      'post' : post,
+                      'form' : form,
+                      'comment' : comment
+                  })
     
